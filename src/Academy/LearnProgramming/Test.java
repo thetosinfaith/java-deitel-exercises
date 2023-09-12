b738b453947818a78bc7e10495f49180c54be9c9
Oluwatosin Faith Ogungbaye <hellotosinfaith@gmail.com>
2023-09-12 17:45:02 +0100

    task commit

---
+++ src/Academy/LearnProgramming/Test.java
@@ -0,0 +1,12 @@
+package Academy.LearnProgramming;
+
+public class Test {
+    public static void main(String[] args) {
+
+        int name = 5;
+        int number = 5;
+
+        System.out.println(name*number);
+    }
+}
+
