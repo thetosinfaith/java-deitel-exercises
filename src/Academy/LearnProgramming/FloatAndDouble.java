0a4161b29e5a392b08f352a125a4e71f2e005a25
Oluwatosin Faith Ogungbaye <hellotosinfaith@gmail.com>
2023-09-13 13:18:23 +0100

    calcuating

--- src/Academy/LearnProgramming/FloatAndDouble.java
+++ src/Academy/LearnProgramming/FloatAndDouble.java
@@ -4,7 +4,7 @@ public class FloatAndDouble {
     public static void main(String[] args) {
         float myMinFloatValue = Float.MIN_VALUE;
         float myMaxFloatValue = Float.MIN_VALUE;
-        System.out.println("float mininum value = " + myMinFloatValue);
+        System.out.println("float minimum value = " + myMinFloatValue);
         System.out.println("float maximum value =" + myMaxFloatValue);
 
         double myMinDoubleValue = Double.MIN_VALUE;
@@ -12,6 +12,13 @@ public class FloatAndDouble {
         System.out.println("double minimum value =" + myMinDoubleValue);
         System.out.println("double maximum value =" + myMaxDoubleValue);
 
+        int myIntValue = 5 / 2;
+        float myFloatValue = 5f / 2f;
+        double myDoubleValue = 5d / 2d;
+
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("myFloatValue  = " + myFloatValue);
+        System.out.println("myDoubleValue = " + myDoubleValue);
 
     }
 }
