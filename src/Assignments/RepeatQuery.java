17fd3a03895d69ba4c5f3425613ffcd8b9038603
Oluwatosin Faith Ogungbaye <hellotosinfaith@gmail.com>
2023-09-14 18:19:46 +0100

    check min and max value

--- src/Assignments/RepeatQuery.java
+++ src/Assignments/RepeatQuery.java
@@ -1,25 +1,27 @@
 package Assignments;
 
+import java.util.Objects;
 import java.util.Scanner;
 
-public class AdditionRepeatQuery {
+public class RepeatQuery {
 
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
 
         String response;
+
         do {
-            System.out.println("Enter first number");
+            System.out.print("Enter first number: ");
             int firstInput = input.nextInt();
 
-            System.out.println("Enter second number");
+            System.out.println("Enter second number: ");
             int secondinput = input.nextInt();
 
             int sum = firstInput + secondinput;
             System.out.println("The sum is: " + sum);
 
-            System.out.println("Do you want to perform this action again?) (Yes/No?");
+            System.out.println("Do you want to perform this action again?) Yes/No?");
             response = input.nextLine();
 
         } while (response == "Yes");
---
+++ src/ChapterFour/VariableSum.java
@@ -0,0 +1,2 @@
+package ChapterFour;public class VariableSum {
+}
