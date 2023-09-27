0dfd3098d3a9d1d1cb3189d2801768e01ec2b1d1
Oluwatosin Faith Ogungbaye <hellotosinfaith@gmail.com>
2023-09-27 22:01:42 +0100

    uploading

--- src/DeitelJavaProgrammingBook/ChapterTwo/LargestAndSmallestIntegers.java
+++ src/DeitelJavaProgrammingBook/ChapterTwo/LargestAndSmallestIntegers.java
@@ -1,26 +1,28 @@
 package DeitelJavaProgrammingBook.ChapterTwo;
 
 import java.util.Scanner;
+
 public class LargestAndSmallestIntegers {
     public static void main(String[] args) {
+        int smallest = Integer.MAX_VALUE;
+        int largest = Integer.MIN_VALUE;
 
         Scanner input = new Scanner(System.in);
 
         for (int i = 0; i < 5; i++) {
+            System.out.print("Enter a number: ");
+            int userInput = input.nextInt();
 
-            System.out.println("Enter a number");
+            if (userInput > largest) {
+                largest = userInput;
+            }
 
-            int largest = userInput;
-            int smallest = userInput;
+            if (userInput < smallest) {
+                smallest = userInput;
+            }
+        }
 
-            userInput  = input.nextInt();
-
-        int largest = ;
-        int smallest = 0;
-
-        System.out.println();
-
-
-
+        System.out.println("Largest number is: " + largest);
+        System.out.println("Smallest number is: " + smallest);
     }
 }
