da61ae575d716017e51fab5ea967f7e2f052e5f5
Oluwatosin Faith Ogungbaye <hellotosinfaith@gmail.com>
2023-10-01 16:55:19 +0100

    Add tests for thetestElementExistInAnArray()function

--- src/Assignments/ArrayOperations.java
+++ src/Assignments/ArrayOperations.java
@@ -15,4 +15,50 @@ public class ArrayOperations {
 
         return largest;
     }
+
+    public static void reverseElementFunction(int[] arrayOperations) {
+
+        int left = 0;   //
+        int right = arrayOperations.length - 1;
+
+        while (left < right) {
+            int temporaryVariable = arrayOperations[left];
+            arrayOperations[left] = arrayOperations[right];
+            arrayOperations[right] = temporaryVariable;
+
+            left++;
+            right--;
+        }
+
+        for (int i = 0; i < arrayOperations.length; i++) {
+            System.out.println(arrayOperations[i] + " ");
+
+        }
+
+    }
+
 }
+//                // Initialize variables
+//                leftIndex = 0            // Index of the first element
+//        rightIndex = length(anArray) - 1  // Index of the last element
+//
+//        // Loop until the indices meet in the middle
+//        while leftIndex < rightIndex
+//                // Swap elements at leftIndex and rightIndex
+//                temp = anArray[leftIndex]
+//        anArray[leftIndex] = anArray[rightIndex]
+//        anArray[rightIndex] = temp
+//
+//        // Move the indices inward
+//        leftIndex = leftIndex + 1
+//        rightIndex = rightIndex - 1
+//
+//        // Array is now reversed
+//
+//// Example Usage
+//        anArray = [9, 7, 10, 55, 273, 46]
+//        ArrayReverser.reverseArray(anArray)
+//// Now, 'anArray' contains [46, 273, 55, 10, 7, 9]
+
+
+
--- test/ArrayOperationsTest.java
+++ test/ArrayOperationsTest.java
@@ -1,7 +1,7 @@
 import Assignments.ArrayOperations;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ArrayOperationsTest {
     @Test
@@ -12,8 +12,75 @@ public class ArrayOperationsTest {
         int largest = ArrayOperations.findElementFunction(largestElementInArray);
 
         assertEquals(273, largest);
+    }
+   ; @Test
+        public void testCorrectlyReversesAnArray(){
 
+            int[] reverseArray = {9, 7,10, 55, 273, 46};
+
+            ArrayOperations.reverseElementFunction(reverseArray);
+
+            int[] expectedArrayReverse = {46, 273, 55, 10, 7, 9};
+
+            assertArrayEquals(expectedArrayReverse, reverseArray);
+    }
+    @Test
+    public void testElementExistInAnArray(){
+
+       int [] array = {9, 7,10, 55, 273, 46};
+
+       int pickOut = 55;
+
+       boolean confirm = doesThisArrayExist(array, pickOut);
+
+       assertTrue(confirm);
+
+    }
+
+
+    @Test
+    public void testFunctionPrintsElementsAtOddPositionsCorrectly(){
+
+    }
+
+    @Test
+    public void testReturnElementsOnEvenPositions(){
+
+    }
+
+    @Test
+    public void testThatFunctionCalculatesTheRunningTotalCorrectly(){
 
 
     }
+
+    @Test
+
+    public void testThatFunctionCorrectlyIdentifiesPalindromes(){
+
+    }
+
+    @Test
+    public void testCasesForAllThreeLoopScenariosToComputeTheSumOfNumbersInTheArray(){
+
+    }
+
+    @Test
+
+    public void testThatFunctionCorrectlyConcatenatesTwoArays() {
+
+    }
+
+    @Test
+    public void testThatFunctionCombinesTwoArraysAlternatelyAsExpected(){
+
+}
+
+
+@Test
+    public void testThatConvertNumberToListOfDigits(){
+
+
+    }
+
 }
